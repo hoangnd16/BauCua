ef9708a95c069a3dac6ebfd824a8ea699790e9a8
Hoang Doan <hoang.nguyen.doan@ofatechjsc.com>
2024-01-22 20:35:49 +0700

    first comit

---
+++ BauCua/Module/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BauCua
+//
+//  Created by Hoàng Doãn on 22/01/2024.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = Asset.Colors.red.color
+    }
+
+
+}
+
